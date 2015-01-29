f6d4dd355a285613ee02949581521de9cabb06fe
Thomas Layh <develop@layh.com>
2015-01-29 13:18:35 +0100

    initial commit

---
+++ config.py
@@ -0,0 +1,43 @@
+import os
+
+# getting environments for stuff that can not be modified
+worlds["aoeminecraft"] = os.environ["WORLDS"]
+outputdir = os.environ["OUTPUTDIR"]
+texturepath = os.environ["TEXTURE"]
+
+def playerIcons(poi):
+	if poi['id'] == 'Player':
+		poi['icon'] = "http://overviewer.org/avatar/%s" % poi['EntityId']
+		return "Last known location for %s" % poi['EntityId']
+
+renders["normalrenderday"] = {
+	"world": "aoeminecraft",
+	"title": "Day",
+	"rendermode": smooth_lighting,
+	"dimension": "overworld",
+	"markers": [dict(name="Players", filterFunction=playerIcons, checked=True)]
+}
+
+renders["normalrendernight"] = {
+	"world": "aoeminecraft",
+	"title": "Night",
+	"rendermode": smooth_night,
+	"dimension": "overworld",
+}
+
+renders['biomeover'] = {
+    'world': 'aoeminecraft',
+    'rendermode': [ClearBase(), BiomeOverlay()],
+    'title': "Biomes",
+    'overlay': ['normalrenderday'],
+	"dimension": "overworld",
+}
+
+
+renders['nether'] = {
+    'world': 'aoeminecraft',
+    'rendermode': nether_smooth_lighting,
+    'title': "Nether",
+	"dimension": "nether",
+}
+
